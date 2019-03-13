f299fa9ed9dfebf02c8b169cdaf6b0344b81afe7
dingxizheng <dingxizheng@gmail.com>
2019-03-13 12:13:00 -0400

    fix: has_one_loader not fulfill results

--- lib/model_to_graphql/generators/has_one_relation_resolver_generator.rb
+++ lib/model_to_graphql/generators/has_one_relation_resolver_generator.rb
@@ -12,7 +12,12 @@ module ModelToGraphql
     class HasOneRelationResolverGenerator < GraphQL::Schema::Resolver
 
       def resolve
-        ModelToGraphql::Loaders::HasOneLoader.for(relation.klass, relation.name).load(object.id.to_s)
+        if relation.klass
+          selected_pair = relation.klass.relations.select { |pair| relation.klass < pair[1].klass && pair[1].is_a?(Mongoid::Association::Referenced::BelongsTo) }
+          relation_name = selected_pair[0]
+        end
+        raise StandardError, "Not able to resolve the inverse relation_nmae of has_one relation #{relation.name} on model #{relation.klass}" if relation_name.nil?
+        ModelToGraphql::Loaders::HasOneLoader.for(relation.klass, relation_name).load(object.id.to_s)
       end
 
       def relation
